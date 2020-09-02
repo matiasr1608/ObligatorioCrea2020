0c6d42efead82c59f36077cdc0d0b2f6fc57ba70
matiasr1608 <69040294+matiasr1608@users.noreply.github.com>
2020-09-02 08:58:58 -0300

    second task

--- js/login.js
+++ js/login.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -16,15 +15,3 @@ loginForm.onsubmit = function(e){
 // function onSignIn () {
 //     window.location.href = "index.html";
 // }
-=======
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
-
-
-});
-function onSignIn () {
-    window.location.href = "pagprincipal.html";
-}
->>>>>>> 39366fe64c387d6d689799c19333408871892dd2
